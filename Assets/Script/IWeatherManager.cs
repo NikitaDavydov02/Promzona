24b0a2b93addf28cfaaafee455572bb98b18e31c
Nikita Davydov <nikita.davidov02@yandex.ru>
2022-12-05 01:19:19 +0300

    Initial commit

---
+++ Assets/Script/IWeatherManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWeatherManager
+{
+    ManagerStatus status { get; }
+    void Startup(NetworkService service);
+}
